7450d7b0199b66f969da24f577ecc08db2d8e889
Eaint MM <eaintmyintmon@gmail.com>
2024-03-25 08:03:15 +0000

    env variable

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 let express = require('express');
 let app = express();
 
@@ -6,6 +7,15 @@ app.use("/public", express.static(__dirname + '/public'));
 app.get("/", function (req, res){
     console.log(__dirname);
     res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/json", function (req, res){
+    if (process.env.MESSAGE_STYLE == "uppercase") {
+        res.json({"message": "HELLO JSON"});
+    } else {
+        res.json({"message": "Hello json"});
+    }
+    
 });
 
 
