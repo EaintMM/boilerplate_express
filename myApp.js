8ac82a7f88133d591a4dbd43281bc9b510b99f89
Eaint MM <eaintmyintmon@gmail.com>
2024-03-25 10:00:55 +0000

    Chain Middleware

--- myApp.js
+++ myApp.js
@@ -2,13 +2,22 @@ require('dotenv').config();
 let express = require('express');
 let app = express();
 
+// Static asset
 app.use("/public", express.static(__dirname + '/public'));
 
+// Root middleware
+app.use(function (req, res, next){
+    console.log(req.method + " " + req.path + " - " + req.ip);
+    next();
+});
+
+// HTML file
 app.get("/", function (req, res){
     console.log(__dirname);
     res.sendFile(__dirname + "/views/index.html");
 });
 
+// Env variable
 app.get("/json", function (req, res){
     if (process.env.MESSAGE_STYLE == "uppercase") {
         res.json({"message": "HELLO JSON"});
@@ -17,6 +26,16 @@ app.get("/json", function (req, res){
     }
     
 });
+
+// Chain middleware
+app.get("/now", function (req, res, next){
+    req.time =  new Date().toString();
+    next();
+}, function (req, res){
+    res.send({"time" : req.time});
+});
+
+
 
 
 
